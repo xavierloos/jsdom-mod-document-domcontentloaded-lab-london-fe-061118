b2e651d8fe127f4ea00a21f067b11c64f3733cb0
Steven G. Harms <steven.harms@flatironschool.com>
2018-07-06 15:05:43 -0400

    solution

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-/* code goes here */
+document.addEventListener('DOMContentLoaded', () => {
+  let e = document.querySelector("div#hidden-div");
+  let newVal = parseInt(e.innerText) + 1;
+  e.innerText = newVal
+})
