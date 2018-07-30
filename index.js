35254965a902aad0070debc1732367eb70ef28c4
Javier LÒPEZ RAMOS <xavier.loos04@gmail.com>
2018-07-30 18:58:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 /* code goes here */
-document.addEventListener("DOMContentLoaded", e => {
-  document.querySelector("body")
-   .addEventListener("click", e => console.log("Reggae, Reggae!"));
-});
+
 
 document.addEventListener("DOMContentLoaded", e => {
 document.querySelector("div#hidden-div").innerText("click", e => console.log("301"));
