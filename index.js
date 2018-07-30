59ad2aa8a3ab5c800f0c59cc896a82e59e2259a2
Javier LÒPEZ RAMOS <xavier.loos04@gmail.com>
2018-07-30 18:57:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 /* code goes here */
 document.addEventListener("DOMContentLoaded", e => {
-document.querySelector("div#hidden-div").innerText("click", e => console.log("301")}
-}
+  document.querySelector("body")
+   .addEventListener("click", e => console.log("Reggae, Reggae!"));
+});
+
+document.addEventListener("DOMContentLoaded", e => {
+document.querySelector("div#hidden-div").innerText("click", e => console.log("301"));
+});
 
