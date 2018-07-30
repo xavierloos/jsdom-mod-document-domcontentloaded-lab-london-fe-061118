fe04b6510145b60e43f6d71f0bbb9c6b34b06f1d
Javier LÒPEZ RAMOS <xavier.loos04@gmail.com>
2018-07-30 18:53:19 +0000

    automatically backed up by learn

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ document.addEventListener("DOMContentLoaded", e => {
   document.querySelector("body")
    .addEventListener("click", e => console.log("Reggae, Reggae!"));
 });
-document.querySelector("div#hidden-div").innerText("click", e => console.log("Reggae, Reggae!"));
+document.querySelector("div#hidden-div").innerText("click", e => console.log("301"));
 
