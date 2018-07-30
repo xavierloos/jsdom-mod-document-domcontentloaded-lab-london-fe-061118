4afd2a0b2b7dafb8f8759eff77273e0843407652
Javier LÒPEZ RAMOS <xavier.loos04@gmail.com>
2018-07-30 18:43:04 +0000

    automatically backed up by learn

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ document.addEventListener("DOMContentLoaded", e => {
   document.querySelector("body")
    .addEventListener("click", e => console.log("Reggae, Reggae!"));
 })
+
+<!Doctype html>
+<html>
+<head>
+  <title> My Site </title>
+  <script src="script.js"></script>
+</head>
+<body>
+  <p id="text">Eventing in JavaScript makes things look sharp.</p>
+</body>
+</html>
